0c5c7fb28ec7b87ba39fb859983723e84f12069d
Thom Lamb <fitnessevangelist@gmail.com>
2019-02-09 11:58:00 -0500

    Basic google auth works

--- rostr/src/App.js
+++ rostr/src/App.js
@@ -4,21 +4,21 @@ import './App.css';
 import {auth} from './Firebase.js';
 import Button from '@material-ui/core/Button';
 import AppBar from './appBar/OurAppBar.js';
-import ClientCard from './clientStack/ClientCard.js';
+import ClientTable from './clientTable/ClientTable.js';
 import SignIn from './auth/SignIn.js';
 
 const StatusEnum = Object.freeze({"Green":1, "Yellow":2, "Red":3})
 
 
 class App extends Component {
-  componentDidMount() {
-    auth.onAuthStateChanged((user) =>{
-      if(user) {
-        this.setState({user});
-
-      }
-    })
-  }
+  // componentDidMount() {
+  //   auth.onAuthStateChanged((user) =>{
+  //     if(user) {
+  //       this.setState({user});
+  //
+  //     }
+  //   })
+  // }
 
 
   render() {
@@ -28,13 +28,8 @@ class App extends Component {
 
         />
 
-        <SignIn/>
-        <ClientCard
-          name="Steve"
-          status={StatusEnum.Green}
-          message="Hi from Steve"
-
-        />
+         <SignIn/>
+       <ClientTable/>
       </div>
     );
   }
